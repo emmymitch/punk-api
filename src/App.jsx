c2c1fda12927bbdd3c9d343acdfadc3ecbf52882
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-22 12:20:11 +0100

    Beer array instead of single

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import BeerCard from "./components/BeerCard/BeerCard";
 
+import beers from "./data/beers.js";
+
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Nav />
       <div className='cards-section cards-section__background'>
-        <BeerCard />
+        <BeerCard beerList={beers}/>
       </div>
     </div>
   );
