d73567432032a0c959ca0ff5cb16d88355a254c0
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-22 11:12:01 +0100

    Adjusted background rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
+import BeerCard from "./components/BeerCard/BeerCard";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Nav />
-      <div className='cards-section'></div>
+      <div className='cards-section cards-section__background'>
+        <BeerCard />
+      </div>
     </div>
   );
 }
