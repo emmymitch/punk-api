86db0c3555c3e540051b97709d4d9ab3f3c7818a
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-22 12:34:09 +0100

    Added food pairings

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -8,17 +8,31 @@ const BeerCard = ({beerList}) => {
             tagline = tagline.slice(0, tagline.length -1);
         }
         
+        const foodPairings = beer.food_pairing.map((food) => {
+            return <li>{food}</li>;
+        });
+
+
         return(
             <div key={beer.id} className="beer-card">
                 <h1 className="beer-card__name">{beer.name}</h1>
+
                 <div className="beer-card__scroll-section">
                     <h2 className="beer-card__tagline">{tagline}</h2>
                     <img className="beer-card__img" src={beer.image_url} alt={beer.name} />
+                    
                     <div className="beer-card__content">
                         <p>ABV: {beer.abv} | pH: {beer.ph} | IBU: {beer.ibu}</p>
                         <p>{beer.description}</p>
-                        <p>{beer.food_pairing}</p>
+                        <br />
+                        <div className="beer-card__content--food-pairings">
+                            <p>Pair with:</p>
+                            <ul>
+                                {foodPairings}
+                            </ul>
+                        </div>
                     </div>
+
                 </div>
             </div>
         )
