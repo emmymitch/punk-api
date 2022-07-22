9b1ccb95f60b0b28c1951a5e1a431a13bf92c46b
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-22 11:05:23 +0100

    Initial card

---
+++ src/components/BeerCard/BeerCard.jsx
@@ -0,0 +1,11 @@
+import "./BeerCard.scss";
+
+const BeerCard = () => {
+    return(
+        <div className="beer-card">
+            <h1>This is a card</h1>
+        </div>
+    )
+}
+
+export default BeerCard;
