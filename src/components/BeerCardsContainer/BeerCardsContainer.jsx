646026280f69ec62966b0a9dfd522da8d6e97258
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-25 12:32:56 +0100

    Extracted cards container

---
+++ src/components/BeerCardsContainer/BeerCardsContainer.jsx
@@ -0,0 +1,23 @@
+import "./BeerCardsContainer.scss";
+import BeerCard from "../BeerCard/BeerCard";
+import beers from "../../data/beers";
+
+const BeerCardsContainer = ({beerList, searchTerm, ABVFilter, acidityFilter, bitterFilter, classicFilter}) => {
+
+    if(searchTerm || ABVFilter || acidityFilter || bitterFilter || classicFilter){
+        return(      
+            <section className='cards-section'>
+                <BeerCard beerList={beerList} />
+            </section>
+        );
+
+    } else{
+        return(
+            <section className='cards-section'>
+                <BeerCard beerList={beers} />
+            </section>
+        )
+    }
+}
+
+export default BeerCardsContainer;
