324f8d413d09fb00ce92d9218f6f22a2444bd3a8
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-25 12:40:59 +0100

    Bolded section title

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = ({searchTerm, handleSearchInput, handleABVCheck, handleAcidityCheck,
             
             <SearchBox label="searchBox" searchTerm={searchTerm} handleSearchInput={handleSearchInput} />
 
-            <p>Filters:</p>
+            <p className="bold">Filters:</p>
             <Filter filterLabel="High Alcohol Content (ABV > 6%) "  handleFilter={handleABVCheck} />
             <Filter filterLabel="Classic Range (First brewed before 2010) " handleFilter={handleClassicCheck} />
             <Filter filterLabel="High Acidity (pH < 4) "  handleFilter={handleAcidityCheck} />
