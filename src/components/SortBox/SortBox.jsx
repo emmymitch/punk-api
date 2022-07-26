d337b33169adc97428da905516e420a25a68f586
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-26 15:16:46 +0100

    Styled sort box

--- src/components/SortBox/SortBox.jsx
+++ src/components/SortBox/SortBox.jsx
@@ -1,17 +1,18 @@
+import "./SortBox.scss";
 
 const SortBox = ({options, sortBy}) => {
 
     const optionList = options.map((option) => {
-        return <option value={option}>{option}</option>
+        return <option className="sort-box__option" value={option}>{option}</option>
     })
 
     return (
-        <>
-            <label htmlFor="sorting"><span className="bold">Sort by:</span></label>
-            <select onChange={sortBy} name="sorting">
+        <form className="sort-box">
+            <label htmlFor="sorting"><span className="bold sort-box__label">Sort by:</span></label>
+            <select className="sort-box__list" onChange={sortBy} name="sorting">
                 {optionList}
             </select>
-        </>
+        </form>
 
     )
 };
