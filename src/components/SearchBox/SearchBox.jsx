3016538437c87ea7a5e7cdb7d2efe759c7fb8c90
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-27 15:26:49 +0100

    Added placeholder text

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -5,7 +5,7 @@ const SearchBox = ({searchTerm, handleSearchInput}) => {
   return (
     <form className="search-box">
       <label htmlFor="search" className="search-box__label"><span className="bold">Search:</span></label>
-      <input type="text" name="search" value={searchTerm} onInput={handleSearchInput} className="search-box__input"/>
+      <input type="text" name="search" value={searchTerm} onInput={handleSearchInput} placeholder="Beer Name..." className="search-box__input"/>
     </form>
   );
 };
