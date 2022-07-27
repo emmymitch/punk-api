5d1dd6aa2f8943b82d5fddee46aa129d5907a294
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-27 17:19:53 +0100

    SortBox unit tests

---
+++ src/components/SortBox/SortBox.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SortBox from "./SortBox";
+
+let sortBoxOptions = ["option1", "option2", "option3"];
+let sortBoxNumbers = [2, 6, 3, 8, 4];
+
+const TestSortBox = () => {
+    //For this test, a basic sort is defined as we are testing that it works, not a specific sort
+    const testSort = () => {return sortBoxNumbers = sortBoxNumbers.sort()};
+    return <SortBox options={sortBoxOptions} sort={testSort} label="label" />
+}
+
+it("Should render complete correct sort dropdown", () => {
+    render(<TestSortBox />);
+
+    const sortLabel = screen.getByText("label", {exact: false});
+    const sortInput = screen.getByRole('combobox');
+    const sortOptions = screen.getAllByRole('option');
+
+    expect(sortLabel).toBeInTheDocument();
+    expect(sortInput).toBeInTheDocument();
+    sortOptions.forEach(option => {
+        expect(option).toBeInTheDocument();
+    })
+});
+
+it("Should sort input on selection", () => {
+    render(<TestSortBox />);
+
+    userEvent.selectOptions(screen.getByRole('combobox'), 'option1');
+    
+    expect(sortBoxNumbers).toStrictEqual([2, 3, 4, 6, 8]);
+});
